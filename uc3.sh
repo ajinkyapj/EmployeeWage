1a226476d22a3f4e880237e31dd214361b05be84
ajinkya9933 <55929416+ajinkya9933@users.noreply.github.com>
2020-05-20 15:55:49 +0530

    Full Part time

---
+++ uc3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+isPartTime=1
+isFulltime=2
+empRatePerHr=20;
+randomcheck=$((RANDOM % 2))
+
+if [ $isFulltime -eq $randomcheck ]
+then
+      empHrs=8;
+elif [ $isPartTime -eq $randomcheck ]
+then
+      empHrs=4;
+else
+      empHrs=0;
+fi
+salary=$(($empHrs*$empRatePerHr))
+echo "salary=$salary"
